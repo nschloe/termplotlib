6de0be8f4dd9501071c202614f4b559c5bc9f024
Nico Schlömer <nico.schloemer@gmail.com>
2018-05-31 16:17:55 +0200

    add horizontal print

--- asciihist/main.py
+++ asciihist/main.py
@@ -5,35 +5,84 @@ from __future__ import division, print_function
 import numpy
 
 
-def hist(a, orientation='horizontal', max_width=40, bins=20,
-         show_counts=True, show_xticks=True):
+def hist(a, orientation='vertical', max_width=40, bins=20):
+    if orientation == 'vertical':
+        hist_vertical(a)
+    else:
+        assert orientation == 'horizontal', \
+            'Unknown orientation \'{}\''.format(orientation)
+        hist_horizontal(a, max_width=40, bins=20)
+    return
+
+
+def hist_horizontal(a, max_width=40, bins=20, bar_width=1):
+
+    #    1.4221 -    10.0159 [     3]:
+
     # ax2.hist(a, bins=30, orientation="horizontal")
     counts, bin_edges = numpy.histogram(a, bins=bins)
     averages = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-    max_count = numpy.max(counts)
+    matrix = _get_matrix_of_eighths(counts, max_width, bar_width)
 
-    full = '\u2588'
-    eighths = [
-        '',
-        '\u258f', '\u258e', '\u258d', '\u258c', '\u258b', '\u258a', '\u2589'
+    chars = [
+        ' ',
+        '\u258f', '\u258e', '\u258d', '\u258c', '\u258b', '\u258a', '\u2589',
+        '\u2588'
         ]
 
-    for count, average in zip(counts, averages):
-        width = count / max_count * max_width
-        # Round to 0.125
-        rwidth = int(round(width * 8))
-        num_full_blocks = rwidth // 8
-        remainder = rwidth % 8
-        if show_xticks:
-            print('{:+e}  '.format(average), end='')
-
-        print(num_full_blocks * full + eighths[remainder], end='')
-        if show_counts:
-            fill_width = (
-                max_width - (num_full_blocks + (1 if remainder > 0 else 0))
-                )
-            print(fill_width*' ' + '  {}'.format(count), end='')
-        print()
+    cfmt = '{{:{}d}}'.format(numpy.max([len(str(c)) for c in counts]))
+    efmt = '{:+.2e}'
+    for k, (counts, row) in enumerate(zip(counts, matrix)):
+        print(
+            (efmt + ' - ' + efmt + ' [' + cfmt + ']: {}')
+            .format(
+                bin_edges[k], bin_edges[k+1],
+                counts, ''.join(chars[item] for item in row)
+                ))
 
     return
+
+
+def hist_vertical(a, bins=30, max_height=10, bar_width=2):
+    # ax2.hist(a, bins=30, orientation="horizontal")
+    counts, bin_edges = numpy.histogram(a, bins=bins)
+
+    matrix = _get_matrix_of_eighths(counts, max_height, bar_width)
+
+    chars = [
+        ' ',
+        '\u2581', '\u2582', '\u2583', '\u2584', '\u2585', '\u2586', '\u2587',
+        '\u2588'
+        ]
+
+    # print text matrix
+    for row in matrix:
+        print(''.join(chars[item] for item in row))
+
+    return
+
+
+def _get_matrix_of_eighths(counts, max_size, bar_width):
+    max_count = numpy.max(counts)
+
+    # translate to eighths of a textbox
+    eighths = numpy.array([
+        int(round(count / max_count * max_size * 8))
+        for count in counts
+        ])
+
+    # prepare matrix
+    matrix = numpy.zeros((len(eighths), max_size), dtype=int)
+    for k, eighth in enumerate(eighths):
+        num_full_blocks = eighth // 8
+        remainder = eighth % 8
+        matrix[k, :num_full_blocks] = 8
+        if remainder > 0:
+            matrix[k, num_full_blocks] = remainder
+
+    # Account for bar width
+    matrix = numpy.repeat(
+            matrix, numpy.full(matrix.shape[0], bar_width), axis=0
+            )
+    return matrix
--- test/test_asciihist.py
+++ test/test_asciihist.py
@@ -5,19 +5,19 @@ import numpy
 import asciihist
 
 
-def test_hist():
+def test_horizontal():
+    # numpy.random.seed(123)
+    sample = numpy.random.normal(size=1000)
+    asciihist.hist(sample, orientation='horizontal')
+    return
+
+
+def test_vertical():
     # numpy.random.seed(123)
     sample = numpy.random.normal(size=1000)
     asciihist.hist(sample)
     return
 
 
-def test_hist2():
-    # numpy.random.seed(123)
-    sample = numpy.random.normal(size=(1000, 2))
-    asciihist.hist(sample)
-    return
-
-
 if __name__ == '__main__':
-    test_asciihist()
+    test_horizontal()
