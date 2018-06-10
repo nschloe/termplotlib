c75ba9f375eec71f3d73eb2e43e9b96af46cc5f5
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-10 15:37:25 +0200

    subplot: more border styles

--- asciiplotlib/subplot.py
+++ asciiplotlib/subplot.py
@@ -9,16 +9,57 @@ def subplot_grid(*args, **kwargs):
 
 class SubplotGrid(object):
     def __init__(
-        self, layout, width=80, column_widths=None, border_style=" ", padding=(1, 2)
+        self, layout, width=80, column_widths=None, border_style="thin", padding=(1, 2)
     ):
         assert (
             len(layout) == 2
         ), "layout must be an interable of length 2 (rows and columns)"
 
-        self._border_style = border_style
-        self._border_char = "x"
+        if len(border_style) == 1:
+            self._border_chars = 11 * [border_style]
+        else:
+            self._border_chars = {
+                "thin": [
+                    "\u2500",  # horizontal
+                    "\u2502",  # vertical
+                    "\u250c",  # top left corner
+                    "\u2510",  # top right corner
+                    "\u2514",  # bottom left corner
+                    "\u2518",  # bottom right corner
+                    "\u251c",  # T left
+                    "\u2524",  # T right
+                    "\u252c",  # T
+                    "\u2534",  # T bottom
+                    "\u253c",  # cross
+                ],
+                "thick": [
+                    "\u2501",
+                    "\u2503",
+                    "\u250f",
+                    "\u2513",
+                    "\u2517",
+                    "\u251b",
+                    "\u2523",
+                    "\u252b",
+                    "\u2533",
+                    "\u253b",
+                    "\u254b",
+                ],
+                "double": [
+                    "\u2550",
+                    "\u2551",
+                    "\u2554",
+                    "\u2557",
+                    "\u255a",
+                    "\u255d",
+                    "\u2560",
+                    "\u2563",
+                    "\u2566",
+                    "\u2569",
+                    "\u256c",
+                ],
+            }[border_style]
 
-        # border_width = {" ": 1}[border_style]
         border_width = 1
         self._num_borders = layout[1] + 1
 
@@ -26,9 +67,12 @@ class SubplotGrid(object):
 
         if column_widths is None:
             self._column_widths = [
-                (self._width - self._num_borders * border_width) // layout[1] for _ in range(layout[1])
+                (self._width - self._num_borders * border_width) // layout[1]
+                for _ in range(layout[1])
             ]
-            for k in range((self._width - self._num_borders * border_width) % layout[1]):
+            for k in range(
+                (self._width - self._num_borders * border_width) % layout[1]
+            ):
                 self._column_widths[k] += 1
         else:
             assert len(column_widths) == layout[1]
@@ -47,11 +91,21 @@ class SubplotGrid(object):
     def get_string(self):
         string = []
         total_width = self._num_borders + sum(self._column_widths)
-        string += [self._border_char * total_width]
 
+        string += [
+            self._border_chars[2]
+            + self._border_chars[8].join(
+                [s * self._border_chars[0] for s in self._column_widths]
+            )
+            + self._border_chars[3]
+        ]
+
+        # collect the subfigure rows
+        srows = []
         for row in self._subfigures:
             cstrings = [item.get_string().split("\n") for item in row]
             max_num_lines = max(len(item) for item in cstrings)
+            pp = []
             for k in range(max_num_lines):
                 p = []
                 for j, cstring in enumerate(cstrings):
@@ -59,16 +113,38 @@ class SubplotGrid(object):
                         s = cstring[k]
                     except IndexError:
                         s = ""
-                    if len(s) > self._column_widths[j]:
+                    # truncate or extend with spaces to match teh column width
+                    if len(s) >= self._column_widths[j]:
                         s = s[: self._column_widths[j]]
-                    elif len(s) < self._column_widths[j]:
+                    else:
                         s += " " * (self._column_widths[j] - len(s))
                     p.append(s)
+                pp.append(
+                    self._border_chars[1]
+                    + self._border_chars[1].join(p)
+                    + self._border_chars[1]
+                )
+            srows.append("\n".join(pp))
 
-                string += [
-                    self._border_char + self._border_char.join(p) + self._border_char
-                ]
-            string += [self._border_char * total_width]
+        intermediate_border_row = (
+            "\n" +
+            self._border_chars[6]
+            + self._border_chars[10].join(
+                [s * self._border_chars[0] for s in self._column_widths]
+            )
+            + self._border_chars[7]
+            + "\n"
+        )
+        string += [intermediate_border_row.join(srows)]
+
+        # final row
+        string += [
+            self._border_chars[4]
+            + self._border_chars[9].join(
+                [s * self._border_chars[0] for s in self._column_widths]
+            )
+            + self._border_chars[5]
+        ]
 
         return "\n".join(string)
 
--- test/test_subplot.py
+++ test/test_subplot.py
@@ -4,7 +4,23 @@ import asciiplotlib as apl
 
 
 def test_subplot():
-    grid = apl.subplot_grid((1, 2), border_style="|", width=20)
+    grid = apl.subplot_grid((1, 2), width=20)
+    grid[0, 0].aprint("Some text")
+    grid[0, 1].aprint("Some more text")
+    string = grid.get_string()
+    assert (
+        string
+        == """┌─────────┬────────┐
+│         │        │
+│  Some   │  Some  │
+│         │        │
+└─────────┴────────┘"""
+    )
+    return
+
+
+def test_subplot_custom_border():
+    grid = apl.subplot_grid((1, 2), border_style="x", width=20)
     grid[0, 0].aprint("Some text")
     grid[0, 1].aprint("Some more text")
     string = grid.get_string()
@@ -20,37 +36,37 @@ xxxxxxxxxxxxxxxxxxxx"""
 
 
 def test_subplot2():
-    grid = apl.subplot_grid((1, 2), width=20)
+    grid = apl.subplot_grid((1, 2), width=20, border_style="thick")
     grid[0, 0].aprint("Some text")
     grid[0, 1].aprint("Some more text\nand more")
     string = grid.get_string()
     assert (
         string
-        == """xxxxxxxxxxxxxxxxxxxx
-x         x        x
-x  Some   x  Some  x
-x         x  and   x
-x         x        x
-xxxxxxxxxxxxxxxxxxxx"""
+        == """┏━━━━━━━━━┳━━━━━━━━┓
+┃         ┃        ┃
+┃  Some   ┃  Some  ┃
+┃         ┃  and   ┃
+┃         ┃        ┃
+┗━━━━━━━━━┻━━━━━━━━┛"""
     )
     return
 
 
 def test_subplot_padding():
-    grid = apl.subplot_grid((1, 2), width=20, padding=2)
+    grid = apl.subplot_grid((1, 2), width=20, padding=2, border_style="double")
     grid[0, 0].aprint("Some text")
     grid[0, 1].aprint("Some more text\nand more")
     string = grid.get_string()
     assert (
         string
-        == """xxxxxxxxxxxxxxxxxxxx
-x         x        x
-x         x        x
-x  Some   x  Some  x
-x         x  and   x
-x         x        x
-x         x        x
-xxxxxxxxxxxxxxxxxxxx"""
+        == """╔═════════╦════════╗
+║         ║        ║
+║         ║        ║
+║  Some   ║  Some  ║
+║         ║  and   ║
+║         ║        ║
+║         ║        ║
+╚═════════╩════════╝"""
     )
     return
 
@@ -64,21 +80,22 @@ def test_subplot_3x2():
     grid[1, 1].aprint("Some more text\nand more")
     grid[1, 2].aprint("Some text")
     string = grid.get_string()
+
     assert (
         string
-        == """xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-x            x            x            x
-x  Some tex  x  Some mor  x  Some mor  x
-x            x  and more  x  and more  x
-x            x            x  even mor  x
-x            x            x            x
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-x            x            x            x
-x  Some mor  x  Some mor  x  Some tex  x
-x  and more  x  and more  x            x
-x  even mor  x            x            x
-x            x            x            x
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"""
+        == """┌────────────┬────────────┬────────────┐
+│            │            │            │
+│  Some tex  │  Some mor  │  Some mor  │
+│            │  and more  │  and more  │
+│            │            │  even mor  │
+│            │            │            │
+├────────────┼────────────┼────────────┤
+│            │            │            │
+│  Some mor  │  Some mor  │  Some tex  │
+│  and more  │  and more  │            │
+│  even mor  │            │            │
+│            │            │            │
+└────────────┴────────────┴────────────┘"""
     )
     return
 
