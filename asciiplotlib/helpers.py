9cc1281a91025fdb3e7e8cdc8e6709971368588d
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-12 00:40:18 +0200

    add missing file

---
+++ asciiplotlib/helpers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+
+
+def create_padding_tuple(padding):
+    # self._padding is a 4-tuple: top, right, bottom, left (just like CSS)
+    if isinstance(padding, int):
+        out = (padding, padding, padding, padding)
+    else:
+        if len(padding) == 1:
+            out = (padding[0], padding[0], padding[0], padding[0])
+        elif len(padding) == 2:
+            out = (padding[0], padding[1], padding[0], padding[1])
+        elif len(padding) == 3:
+            out = (padding[0], padding[1], padding[2], padding[1])
+        else:
+            assert len(padding) == 4
+            out = (padding[0], padding[1], padding[2], padding[3])
+    return out
