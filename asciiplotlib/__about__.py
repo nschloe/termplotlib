f64f76d851dbbb83ea35677e169e5e4c36dffce2
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-29 17:26:02 +0200

    version bump

--- asciiplotlib/__about__.py
+++ asciiplotlib/__about__.py
@@ -5,5 +5,5 @@ __author__ = u"Nico Schlömer"
 __email__ = "nico.schloemer@gmail.com"
 __copyright__ = u"Copyright (c) 2018, {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __status__ = "Development Status :: 4 - Beta"
