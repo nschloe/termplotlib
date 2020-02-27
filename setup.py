ce521eb57dea47dcc7ffb0ced3f208b24657bf35
Nico Schlömer <nico.schloemer@gmail.com>
2020-02-27 14:44:42 +0100

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import codecs
 import os
 
 from setuptools import find_packages, setup
@@ -10,10 +9,6 @@ with open(os.path.join(base_dir, "termplotlib", "__about__.py"), "rb") as f:
     exec(f.read(), about)
 
 
-def read(fname):
-    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
-
-
 setup(
     name="termplotlib",
     version=about["__version__"],
@@ -23,7 +18,7 @@ setup(
     author_email=about["__email__"],
     install_requires=[],
     description="Plotting on the command line",
-    long_description=read("README.md"),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=about["__license__"],
     python_requires=">=3",
